94a31c9cdc7afa70efd96d67c5a6391214b00143
marandarosa <marandavalentin@aol.com>
2023-12-19 19:01:35 -0500

    Created react weather app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
-import "./App.css";
+import React, { useState } from "react";
 import Weather from "./Weather";
+import "./index.css";
 
-function App() {
+export default function App() {
+  let [city, setCity] = useState(null);
+  let [cityDisplay, setCityDisplay] = useState(null);
+
+  function submitCity(event) {
+    event.preventDefault();
+    setCityDisplay(city);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Weather App</h1>
-        <Weather city="Hartford" />
-      </header>
+      <h1>Weather App</h1>
+      <form onSubmit={submitCity}>
+        <input
+          type="search"
+          placeholder="Enter city name.."
+          onChange={changeCity}
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <br />
+      {cityDisplay && <Weather city={cityDisplay} />}
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather(props) {
-  function doSomething(response) {
-    alert(`The weather in Hartford is ${response.data.main.temp}`);
+  let [temp, setTemp] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [windSpeed, setWindSpeed] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  function displayWeather(response) {
+    setTemp(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWindSpeed(response.data.wind.speed);
+    setIcon(
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    console.log(response.data);
   }
 
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=6643c7326a4c2a38838264a28531d97e&units=imperial`;
-  axios.get(apiUrl).then(doSomething);
-  return <h2>Loading...</h2>;
+  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=6643c7326a4c2a38838264a28531d97e&units=imperial`;
+  axios.get(weatherUrl).then(displayWeather);
+
+  return (
+    <ul className="Weather">
+      <li>Temperature: {Math.round(temp)}°F</li>
+      <li>Description: {description}</li>
+      <li>Humidity: {humidity}%</li>
+      <li>Wind: {Math.round(windSpeed)}mph</li>
+      <img src={icon} />
+    </ul>
+  );
 }
