9fdc17597e816bdf071027fd9837e5a9899d9c62
marandarosa <marandavalentin@aol.com>
2024-01-04 17:51:49 -0500

    Restructured code to simplify and added default city

--- src/App.js
+++ src/App.js
@@ -3,31 +3,11 @@ import Weather from "./Weather";
 import "./index.css";
 
 export default function App() {
-  let [city, setCity] = useState(null);
-  let [cityDisplay, setCityDisplay] = useState(null);
-
-  function submitCity(event) {
-    event.preventDefault();
-    setCityDisplay(city);
-  }
-
-  function changeCity(event) {
-    setCity(event.target.value);
-  }
-
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <form onSubmit={submitCity}>
-        <input
-          type="search"
-          placeholder="Enter city name.."
-          onChange={changeCity}
-        />
-        <input type="submit" value="Search" />
-      </form>
-      <br />
-      {cityDisplay && <Weather city={cityDisplay} />}
+      <div className="container">
+        <Weather defaultCity="Hartford" />
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -2,33 +2,63 @@ import React, { useState } from "react";
 import axios from "axios";
 
 export default function Weather(props) {
-  let [temp, setTemp] = useState(null);
-  let [description, setDescription] = useState(null);
-  let [humidity, setHumidity] = useState(null);
-  let [windSpeed, setWindSpeed] = useState(null);
-  let [icon, setIcon] = useState(null);
+  let [city, setCity] = useState(props.defaultCity);
+  let [weatherData, setWeatherData] = useState({ ready: false });
 
   function displayWeather(response) {
-    setTemp(response.data.main.temp);
-    setDescription(response.data.weather[0].description);
-    setHumidity(response.data.main.humidity);
-    setWindSpeed(response.data.wind.speed);
-    setIcon(
-      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
-    );
-    console.log(response.data);
+    setWeatherData({
+      city: response.data.name,
+      temp: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      windSpeed: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      ready: true,
+    });
   }
 
-  let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=6643c7326a4c2a38838264a28531d97e&units=imperial`;
-  axios.get(weatherUrl).then(displayWeather);
+  function submitCity(event) {
+    event.preventDefault();
+    search();
+  }
 
-  return (
-    <ul className="Weather">
-      <li>Temperature: {Math.round(temp)}°F</li>
-      <li>Description: {description}</li>
-      <li>Humidity: {humidity}%</li>
-      <li>Wind: {Math.round(windSpeed)}mph</li>
-      <img src={icon} alt={description} />
-    </ul>
-  );
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  function search() {
+    let weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6643c7326a4c2a38838264a28531d97e&units=imperial`;
+    axios.get(weatherUrl).then(displayWeather);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <h1>Weather App</h1>
+        <form onSubmit={submitCity}>
+          <input
+            type="search"
+            placeholder="Enter city name.."
+            autofocus="on"
+            onChange={changeCity}
+          />
+          <input type="submit" value="Search" />
+        </form>
+        <br />
+        <ul>
+          <li>
+            <strong>{weatherData.city}</strong>
+          </li>
+          <li>Temperature: {Math.round(weatherData.temp)}°F</li>
+          <li>Description: {weatherData.description}</li>
+          <li>Humidity: {weatherData.humidity}%</li>
+          <li>Wind: {Math.round(weatherData.windSpeed)}mph</li>
+          <img src={weatherData.icon} alt={weatherData.description} />
+        </ul>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
